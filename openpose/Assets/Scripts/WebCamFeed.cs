e16bfb822d9e1477efa0f993d7333b1817f6af7a
Felix Lange <felix.lange@ixds.com>
2018-04-29 21:29:03 +0200

    addional scene with webcam feed

---
+++ openpose/Assets/Scripts/WebCamFeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamFeed : MonoBehaviour {
+
+	public RawImage rawimage;
+	bool setSize = false;
+	
+	void Start () 
+	{
+			WebCamTexture webcamTexture = new WebCamTexture();
+			rawimage.texture = webcamTexture;
+			webcamTexture.Play();
+	}
+	
+	void Update()
+	{
+		// rawimage.texture.width
+		rawimage.SetNativeSize();
+	}
+}
