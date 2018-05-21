0d58d737874dc52cef11b7a227d7f9b347799816
Felix Lange <felix.lange@ixds.com>
2018-05-21 08:27:33 +0200

    OpenPose playback Example (via Timeline) added
    Listener bridges between Timeline and OpenPose drawer

---
+++ openpose/receiveOnly/Assets/Scripts/OpenPoseListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using RecordAndPlay;
+
+public class OpenPoseListener : DataListener
+{
+	[System.Serializable]
+	public class StringEvent : UnityEvent<string>{};
+	[SerializeField]
+	public StringEvent updateEvent; 
+	
+    public override void ProcessData(DataFrame data)
+    {
+		StringData stringData = data as StringData;
+		updateEvent.Invoke(stringData.data);
+    }
+}
